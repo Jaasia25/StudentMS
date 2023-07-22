7fc4125d6e598ef2c0a9a3f90cbbd656ab44189c
Jaasia <jaasiaa2019@gmail.com>
2023-07-22 11:40:21 +0600

    homepage-ui-change

--- index.php
+++ index.php
@@ -8,22 +8,57 @@
     <title>Student Management System</title>
 
     <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" 
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/1344c17b50.js"></script>
 
 </head>
 <body>
 
 <!-- home section -->
 
+
+
 <div class="home">
-    <div class="content">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+        <img src="./image/aust.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+        AUST | SMS
+        </a>
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav justify-content-end">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="signin.php">Login</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="signup.php">Sign Up</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+
+    <div class= "student">
+        <center><img src="./image/student.png"></center> 
+    </div>
+
+    <!-- <div class="content">
         <div class="container">
             <h1>Welcome to SMS</h1>
-            <a href="signin.php">Sign In</a>
-            <a href="signup.php">Sign Up</a>
         </div>
 
-    </div>
+    </div> -->
 
 </div>
 
